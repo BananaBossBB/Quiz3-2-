074cee9a113e62804ffea9e141b8eb1e67c9f346
badarzaa <104325948+BananaBossBB@users.noreply.github.com>
2023-04-24 10:31:40 +0700

    1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int backPack = int.Parse(Console.ReadLine());
+
+        for(int i = 0;i < backPack;i++)
+        {
+            string name = Console.ReadLine();
+            string catagory = Console.ReadLine();
+        }
+        
+    }
+}
+
+        
